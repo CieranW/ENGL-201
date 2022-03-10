46da9077ab2ce1a0ce5aa742f467eb0d6e2a0771
CieranW <cieranwong@gmail.com>
2022-03-10 14:01:46 -0700

    Refined the split functions, changed a bunch of variable names for better description

--- experiment3.py
+++ experiment3.py
@@ -5,28 +5,6 @@ def intro():
     input("Throughout the experiment, I will be utilizing the different types of Python functions and programs as shells, and my words/writings as fillers for the aforementioned shells."'\n')
 
 
-def yes_or_no():
-    count = 0
-    yes = "y"
-    no = "n"
-    while count == 0:
-        # Give reader a choice.
-        choice = str(input("So, shall we begin? (Enter y or n to start)"'\n'))
-
-        if choice is not no and choice is not yes:
-            print("Please enter a valid answer."'\n')
-            count += 0
-
-        if choice == no:
-            count += 1
-            print("Sorry to see you go :)")
-            break  # Ends the program.
-
-        if choice == yes:
-            count += 2
-    return choice
-
-
 def main_body():
     input('\n'"Fantastic!"'\n'"Gather around, kids. It's story time!"'\n')
 
@@ -40,8 +18,9 @@ def main_body():
 
 
 def questions():
+    global user_phrase1, user_number1, user_number2, price
     # Store inputted string
-    q1 = str(input(
+    user_phrase1 = str(input(
         "Allow me to demonstrate. Please type a sentence/phrase of your own in the line below:"'\n'))
 
     input('\n'"Taking in your sentence/phrase, I can insert it randomly however I want. I can call it in two sentences from now, or I could call it in at the end of the essay. However, there are certain limitations to how I could implement said sentence, obviously a result in part due to my novicity and limited understanding of the joy of python."'\n')
@@ -49,63 +28,65 @@ def questions():
     input("Let me call on your sentence/phrase now."'\n')
 
     # Calls upon the inputted string from earlier
-    input(q1)
+    input(user_phrase1)
 
     input('\n'"If you had entered it correctly, then your sentence should have shown up after I had typed 'Let me call on your sentence now.'."'\n')
 
     input("But words and sentences are not the only thing I can take in from you and call upon later. Python is capable of doing that with numbers too."'\n')
 
     # Stores number 1
-    n1 = int(
+    user_number1 = int(
         input("Please indulge me and enter a whole number of your own choosing:"'\n'))
     # Stores number 2
-    n2 = int(input('\n'"Now enter a second number:"'\n'))
+    user_number2 = int(input('\n'"Now enter a second number:"'\n'))
 
     input("Why don't we take those two numbers and turn it into an equation?"'\n')
     input("Try a simple x^2 + y^2."'\n')
     input("Taking in your numbers from earlier, we should get the following:"'\n')
 
     # x^2 + y^2
-    a1 = (n1*n1) + (n2*n2)
-    input(a1)
+    answer1 = (user_number1*user_number1) + (user_number2*user_number2)
+    number_combo1 = user_number1*user_number1
+    number_combo2 = user_number2*user_number2
+    input(answer1)
 
-    input("As shown, {n1}^2 should give us {0}, and {n2}^2 is {1}. And together they give us {a1}.".format(
-        (n1*n1), (n2*n2)))
+    input("As shown, {0}^2 should give us {1}, and {2}^2 is {3}. And together they give us {4}.".format(
+        user_number1, (number_combo1), user_number2, (number_combo2), answer1))
 
     input('\n'"We can even take in your numbers from earlier and put it into a different type of equation, say a tax calculator."'\n')
 
     # Calculate tax based on numbers 1 and 2
-    tax = round((n1 * (1 + (n2/100))), 2)
+    tax = round((user_number1 * (1 + (user_number2/100))), 2)
 
     # Using the tax formula, calculate price and round to two decimal places
-    price = round((n1*n2)*(1+(n2/100)), 2)
+    price = round((user_number1*user_number2)*(1+(user_number2/100)), 2)
 
-    input("Using the first number as your initial price (${n1}), and your second number as your tax percentage ({1}), we can get the following:".format(
-        round((n2/100), 2)))
+    input("Using the first number as your initial price (${0}), and your second number as your tax percentage ({1}), we can get the following:".format(
+        user_number1, round((user_number2/100), 2)))
 
-    input('\n''\t'"Your final price is: ${tax}.")
+    input('\n''\t'"Your final price is: ${0}.".format(tax))
 
     input(
-        '\n'"This is done through the following calculation: ${n1} x (1+({n2}/100)) = ${tax}."'\n')
-    return q1, n1, n2, price
+        '\n'"This is done through the following calculation: ${0} x (1+({1}/100)) = ${2}."'\n'.format(user_number1, user_number2, tax))
 
 
 def story_time():
+    # user_phrase1, user_number1, user_number2, price = questions()
     input("Let's put your sentence and numbers to good use."'\n')
 
     # Story time, takes in the string and numbers inputted by reader and turns it into a story.
     input('\t'"A man walks into a small, quaint shop that sells doughnuts (I'm craving doughnuts as I'm typing this).")
     input(
-        "He exclaims, 'How much for one, kind sir?', to which the owner replies, 'Why my good man, it is ${n1} a piece.'")
-    input("'I will take {n2} then', the man remarks.")
+        "He exclaims, 'How much for one, kind sir?', to which the owner replies, 'Why my good man, it is ${0} a piece.'".format(user_number1))
+    input("'I will take {0} then', the man remarks.".format(user_number2))
     input(
-        "'Fantastic choice, lad. Truly worth every penny. Your total for today is ${price}, tax included, of course.'")
+        "'Fantastic choice, lad. Truly worth every penny. Your total for today is ${0}, tax included, of course.'".format(price))
     input(
-        "'Wonderful, my good lord. {q1} I bid thee good day.'")
+        "'Wonderful, my good lord. {0} I bid thee good day.'".format(user_phrase1))
     input('\t'"And so ends our wonderful story of a man in a doughnut shop."'\n')
 
 
-def ending():
+def ending1():
     no = "n"
     yes = "y"
     # Loop to run program again.
@@ -114,16 +95,39 @@ def ending():
         input("Thank you for coming along on my little adventure. Whether or not I will attempt to do this again remains to be seen, depending on the grade I get. Although I perhaps may not have followed the prompt assigned, it was my ambition to use a programming language as a medium or an avenue for my essay. Regardless of the result, I have had quite the time writing this, which is nothing to laugh at as I have spent a deal greater than I would have should I have stuck with writing it out in Microsoft Word. There were many challenges that I have faced, bugs to debug, and formats to change, many of which, given the time I had have not solved nor figured out. An example being getting the paragraphs and sentences to format correctly in the terminal. Nevertheless, I very much enjoyed doing this and would hope to commit in the future."'\n')
 
         input('\t'"The End."'\t')
+
         # break
 
     if q2 == yes:
         count -= 2
 
 
-# Calls upon the function, main
+def ending2():
+    input("Thanks for playing along and being a good sport. Have a good day!")
+
+
+# Calls upon the functions in order of how they should be presented or the questions.
 intro()
-yes_or_no()
-main_body()
-questions()
-story_time()
-ending()
+count = 0
+yes = "y"
+no = "n"
+while count == 0:
+    # Give reader a choice.
+    choice = str(input("So, shall we begin? (Enter y or n to start)"'\n'))
+
+    if choice is not no and choice is not yes:
+        print("Please enter a valid answer."'\n')
+        count += 0
+
+    if choice == no:
+        count += 1
+        ending2()
+        break  # Ends the program.
+
+    if choice == yes:
+        count += 2
+        main_body()
+        questions()
+        story_time()
+        ending1()
+        ending2()
