c816efdddf1297d7625c2b5ee4b445b830097157
CieranW <cieranwong@gmail.com>
2022-03-16 22:00:26 -0700

    New file

---
+++ workshopessay.py
@@ -0,0 +1,20 @@
+def intro():
+    input("Guess I'm doing this again."'\n')
+    input("However, this time I'll try to incorporate different elements that can be controlled, such as the type of font and size, color of font, and spacing/text appearance."'\n')
+    input("This should be fun."'\n')
+
+
+def essay1():
+    pass
+
+
+def essay2():
+    pass
+
+
+def essay3():
+    pass
+
+
+choose_which_essay_to_read_first = input(
+    "Which essay would you like to read first? '\n'Option a) My first memory '\n'Option b) An objective approach tackling the concept of muscle memory '\n'Option c) ")
