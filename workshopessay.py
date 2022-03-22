98d93b06c1e4923c1d50ecdc463eafcd76ed98d0
CieranW <cieranwong@gmail.com>
2022-03-22 11:19:26 -0700

    Finally worked on the header formatting function as well as the text format.

--- workshopessay.py
+++ workshopessay.py
@@ -3,8 +3,7 @@ import textwrap
 
 def main_function(essay_count):
     if essay_count == 0:
-        print(
-            "Which essay would you like to read first?\n")
+        print(lbb + "Which essay would you like to read first?" + lbb)
         choose_an_essay = essay_selector(essay_choices)
         if essay_choices[choose_an_essay] == "My first memory":
             essay_choices.remove("My first memory")
@@ -25,7 +24,7 @@ def main_function(essay_count):
             main_function(essay_count)
 
     elif essay_count < 3:
-        input("Which essay would you like to read next?\n")
+        print(lbb + "Which essay would you like to read next?" + lbb)
         choose_an_essay = essay_selector(essay_choices)
         if essay_choices[choose_an_essay] == "My first memory":
             essay_choices.remove("My first memory")
@@ -46,17 +45,16 @@ def main_function(essay_count):
             main_function(essay_count)
 
     elif essay_count == 3:
-        print("Thank you for tagging along, I hope you enjoy that little journey.")
+        print("Thank you for tagging along, I hope you enjoyed that little journey.")
         input("Would you like to read the essays again?")
         print("\n (a) Yes, I would like to bore myself some more as I have nothing better to do. \n (b) No, I have better things to do.\n")
-        print(essay_count)
         run_it_back = str(
             input("Indulge me and enter a letter that suits your satisfaction: "))
         a = "a"
         b = "b"
         for value in run_it_back:
             if value == a:
-                print("Wrong choice! Back to the start we go")
+                print("Wrong choice! Back to the start we go.")
                 essay_count -= 3
                 essay_choices.extend(["My first memory",
                                      "The concept of muscle memory", "Why do I do this to myself?"])
@@ -79,6 +77,7 @@ def essay_selector(essay_choices):
 def chooser(essay_choices):
     try:
         selected = int(input("Enter the number of your choice: ")) - 1
+
         if selected >= len(essay_choices):
             print("Invalid input, try again.")
             selected = chooser(essay_choices)
@@ -89,36 +88,48 @@ def chooser(essay_choices):
     return selected
 
 
-def responder():
-    response = int(input("Enter the number of your choice: "))
-    if response not in [1, 2]:
-        print("Please enter a valid number.")
-        responder()
-    return response
+# def responder():
+#     response = int(input("Enter the number of your choice: "))
+#     if response not in [1, 2]:
+#         print("Please enter a valid number.")
+#         responder()
+#     return response
 
 
 def format_headers(header):
-    pass
+    print(lbb + (header.center(24, " ")) + lbb)
 
 
 def format_main_text(text):
-    pass
+    input(textwrap.fill('\n' + text + '\n'))
 
 
 # Essays, introduction, conclusion, and connectors for essays.
 
 
 def intro():
-    input("\nGuess I'm doing this again\n")
-    input("How do I even begin? The last time I did this, I'll admit it was a little rough.\n")
-    input("However, this time I'll try to incorporate different elements that can be controlled, such as the type of font and size, color of font, and spacing/text appearance.\n")
-    input("This should be fun, so hang tight.\n")
+    format_headers("Another One")
+    format_main_text(
+        "How do I even begin? The last time I did this, I'll admit it was a little rough.")
+    format_main_text("However, this time I'll try to incorporate different elements that can be controlled, such as the type of font and size, color of font, and spacing/text appearance.")
+    format_main_text("This should be fun, so hang tight.")
 
 
 def essay1():
     # Title, needs to be centered, bold font, size 12, Times New Roman.
     format_headers("Sexualizing Food")
-    input("Perhaps it needs to be said, the title is incorrect and simply an act of humor, I will not be sexualizing food in this part of the essay. I can not stress that enough.")
+    format_main_text("Perhaps it needs to be said, the title is incorrect and simply an act of humor, I will not be sexualizing food in this part of the essay. I can not stress that enough.")
+    format_main_text("""
+    Greece was gorgeous, but how would I know? For one, I was four years old, and my memory is outright terrible, I can barely recall what happened last week, much less hope to remember events that transpired almost fifteen years ago. However, one thing is clear as day, the food was bloody amazing. Fresh seafood, beautifully grown olives made into sensational olive oil, and so much more. It was heaven for the taste buds.""")
+
+    format_main_text("""
+    Up till that point before I had gone to Greece, I was a rather picky eater, only plain spaghetti would suffice, everything else was a coin toss and my parents never knew if I would eat it. Therefore, it was plausible that there was concern regarding how I would react to a new kind of cuisine on a different continent in a new country. Thankfully, to my parents' delight, I loved the food Greece had to offer; one of my personal favorites that I still crave on a daily basis is grilled octopus.""")
+
+    format_main_text("""
+    The grilled octopus that I had in Greece was an abomination of flavors. Perfectly tender to the point where it melted in your mouth but still kept this degree of bounce and chewiness desired in shellfish and octopus. Finished with a drizzle of fresh, virgin olive oil and a crank of black pepper, at that time in my life, it was the greatest thing I had ever tasted. Greece had an abundant amount of fresh seafood which I have come to enjoy and appreciate, especially in a place landlocked, like Arizona.""")
+
+    format_main_text("""
+    Seafood can be a hit or miss for most people, however, when cooked correctly, the meat becomes a perfect symphony of sweet and salty, an orchestra of delight and wonder. It truly was an eye-opening experience, or rather a mouthwatering experience to taste food that paid homage to the quality and freshness of the catch, while playing around and beautifying it further to enhance the flavor and make it define what Greece and Greek food is. That was my first experience with food that I truly remember to this day, and I have strived to experience and appreciate food for what it is: not a mismatch of ingredients hoping to vomit out a perfect combo, but rather a dish that preserves the quality of the produce, while enhancing it further to broaden one's horizons.""")
 
 
 def essay2():
@@ -138,7 +149,8 @@ essay_titles = ["Sexualizing Food", "The Mind and Body in all its glory",
 essay_choices = ["My first memory",
                  "The concept of muscle memory", "Why do I do this to myself?"]
 
-
+lbb = "\n------------------------------------------------------------\n"
+lb = "------------------------------------------------------------\n"
 essay_count = 0
 # Calling on the functions in order.
 intro()
