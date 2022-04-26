8e50ba9ae993fd7cf8d80aa764747823d5fe2d1d
CieranW <cieranwong@gmail.com>
2022-04-26 09:09:08 -0700

    Added text to essay3, added some comments for further improvements

--- workshopessay.py
+++ workshopessay.py
@@ -64,7 +64,6 @@ def main_function(essay_count):
 # Functions to help connect/modify appearances of main body.
 
 
-# Maybe not use numbers. Have a random function of numbers or symbols.
 def essay_selector(essay_choices):
     for number in range(0, len(essay_choices)):
         print("("+str(number+1)+") " + essay_choices[number] + '\n')
@@ -107,7 +106,6 @@ def intro():
     # Title needs to be changed, something to do with memory. Also, I need a good intro paragraph into memory and how it relates to the essays.
     format_headers("Another One")
     format_main_text("We all have memories, be they good or bad, they define us and made us the people we are today. Our choices, fears, and desires are all influenced by events that have occurred in the past, no matter the level of significance.")
-    # format_main_text("")
     format_main_text("There are many different factors when it comes to remembering something, the significance of the event, importance or usefulness of the knowledge acquired, or how it has affected your life. In this short piece, I attempt to acknowledge these three particular areas of memory and the effects that they have.")
 
 
@@ -138,7 +136,7 @@ def essay2():  # Did not flow with the rest of the piece, need to change how muc
     format_main_text(
         "I present an excerpt of the mind and body string theory: ")
     format_main_text("""
-    We begin by addressing the idea of mind and body. René Descartes, a French philosopher in the 1600s, wrote Meditations on First Philosophy, a series of meditations on first philosophy, with the sixth part being the distinction between mind and body. In the sixth meditation, he mentions that the mind and body are two extended substances with the key difference being the mind is an extended thinking thing and the body is not (Descartes, Cress translation, 98). He further goes on to mention that in order to achieve a complete being — the human person — harmonization between mind and body would be needed as only through achieving this connection would they now no longer be two extended substances but a complete being -- as shown in Descartes' example of a sailor and his ship being two separate substances but together are constituted as one single thing (Descartes, Cress translation, 98). And as they are now conjoined in harmonization, theory would surface that if one extended substance was limited in development, it would inhibit the other from developing as the connection between mind and body is like a theoretical string, one can only get so far ahead of the other before it is pulled back, reaching the limit on the string's pull or worse, snapping, severing the bridge between both extended substances and thus rendering the human body no longer a complete being but two extended substances living in close proximity to one another. It is also applicable that the string could initially be shown as a thin piece of string, our starting weak connection between mind and body, and as we continue to grow and develop, the string increases in layers of connection, eventually becoming a thick rope with a heightened, layered connection. We can conclude from this that the parallel development of mind and body is necessary; sports or physical activities and academics would need to support one another through the developmental stages of a child as they progressed into becoming a young adult. Therefore, as shown earlier, if the mind portion of the human body is being developed through the pursuit of academic excellence, but the body portion is neglected due to external conflicts such as scheduling or parental pressure, then eventually the mind would cease to continue development at the same rate unless changes are made to the developmental rate of the body. This I coin the mind and body string theory. """)  # Expand on the narrator portion.
+    We begin by addressing the idea of mind and body. René Descartes, a French philosopher in the 1600s, wrote Meditations on First Philosophy, a series of meditations on first philosophy, with the sixth part being the distinction between mind and body. In the sixth meditation, he mentions that the mind and body are two extended substances with the key difference being the mind is an extended thinking thing and the body is not (Descartes, Cress translation, 98). He further goes on to mention that in order to achieve a complete being — the human person — harmonization between mind and body would be needed as only through achieving this connection would they now no longer be two extended substances but a complete being -- as shown in Descartes' example of a sailor and his ship being two separate substances but together are constituted as one single thing (Descartes, Cress translation, 98). And as they are now conjoined in harmonization, theory would surface that if one extended substance was limited in development, it would inhibit the other from developing as the connection between mind and body is like a theoretical string, one can only get so far ahead of the other before it is pulled back, reaching the limit on the string's pull or worse, snapping, severing the bridge between both extended substances and thus rendering the human body no longer a complete being but two extended substances living in close proximity to one another. It is also applicable that the string could initially be shown as a thin piece of string, our starting weak connection between mind and body, and as we continue to grow and develop, the string increases in layers of connection, eventually becoming a thick rope with a heightened, layered connection. We can conclude from this that the parallel development of mind and body is necessary; sports or physical activities and academics would need to support one another through the developmental stages of a child as they progressed into becoming a young adult. Therefore, as shown earlier, if the mind portion of the human body is being developed through the pursuit of academic excellence, but the body portion is neglected due to external conflicts such as scheduling or parental pressure, then eventually the mind would cease to continue development at the same rate unless changes are made to the developmental rate of the body. This I coin the mind and body string theory. """)  # Expand on the narrator portion. Check notes/feedback forms.
     format_main_text("""
     I present a visualization and metaphor for the mind and body string theory to better aid you: we begin with two runners running in a straight line at a constant pace and a piece of string connecting the two runners to one another, if one should falter and change his pace, the string will soon find maximum tension and the other will be forced to change his pace to match the first runner. Think of it this way, if one runner were to increase or decrease his pace, eventually the string must find maximum tension, thus resulting in the runner in front being unable to widen the distance between him and the second runner. This tension would remain present until either the runner ahead decreases his pace, or the runner behind increases his pace; if not done as so, the runner in front would have to work harder to pull the runner behind along, a scenario that would quickly result in fatigue and burnout. If they both were to progress at the same rate, it would be logical that they will eventually have a higher form of connection such as compatibility to each one's unique assets. A scenario for the development of mind and body: if our body is developed but our minds are left behind, eventually, we will reach a point where development of the body will slow down until the development of the mind catches up; and if the mind does not catch up, the body will slowly burn out. And if forced to continue at the same pace, the substance in front would eventually burn out from extended fatigue and exhaustion from pulling the other substance along. """)
     format_main_text("""
@@ -174,13 +172,12 @@ def essay3():  # Expand upon.
         write a full-on essay in a code editor, as I found out,
         not exactly the smartest thing to do as there were plenty of challenges
         that I encountered along the way. The hardest part about writing an 
-        essay in a programming language
-        is getting it to run as smooth as you envision it to. 
-        Nevertheless, I had extra time on my hand and I had already made that
-        initial commitment, I was not backing down from this, nor would I stop 
-        till it was complete. The beauty of writing in code is seeing the 
-        fruits of your labor. When it runs perfectly, the satisfaction is 
-        immeasurable.")
+        essay in a programming language is getting it to run as smooth as you 
+        envision it to. Nevertheless, I had extra time on my hand and I had 
+        already made that initial commitment, I was not backing down from this, 
+        nor would I stop till it was complete. The beauty of writing in code is 
+        seeing the fruits of your labor. When it runs perfectly, the 
+        satisfaction is immeasurable.")
     """)
     input("""
     def second_part():
@@ -191,8 +188,37 @@ def essay3():  # Expand upon.
         as well as ensuring that the flow made sense. In short, the essay 
         was a description of how I built the program and the challenges I faced.
         ")
-        format_main_text(")
+        format_main_text("No doubt a tedious experience, in order to ensure 
+        that I had the correct format throughout the entire essay,
+        I had to continuously run it through the terminal so that I could 
+        visually see the results and the fruits of my labor come to life.
+        Another big challenge that I encountered which made this whole 
+        experience rather difficult, was the backbone, or the shell of the
+        program. In this particular program, of which I am currently editing,
+        I have about 8 different functions and variables dedicated to 
+        the backbone of my program. Obviously, when running the program, the 
+        reader does not see this backbone at all, rather they are witness to 
+        the results of the backbone, hopefully, a fluid flow of words that 
+        make sense.")
+        format_main_text("Coming back to the first time I wrote an essay in a 
+        code editor, I had mentioned that it was for practice, trying to 
+        put theory into application, a usage of memory. Memory plays a large 
+        role in the world of programming; you often times need to know how 
+        much memory your program will use up, how much memory it takes to keep 
+        it running, and how much memory can it process when handling large 
+        sets of data. These are all factors that we need to look at when 
+        writing code. However, in this instance, memory is also a mental 
+        attribute. It can be difficult at times to know the correct 
+        functions to use, where certain variables should be placed,
+        or when a function should be called. Correct visualization of 
+        the project is needed, and more often than not, I would 
+        find myself writing it out on a piece of paper before 
+        applying it on the computer.")
     """)
+    input("""
+    def third_part():
+        format_main_text("") 
+    """)  # Need to come back and brainstorm again. Lost the direction for a bit.
 
 
 def finisher():
